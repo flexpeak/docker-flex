04404b46343d93d0230b152fbdd066de9fb5da07
flexpeak <59660805+flexpeak@users.noreply.github.com>
2022-07-28 14:52:27 -0400

    Create ecosystem.config.js

---
+++ node/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = [{
+  script: '/home/flexdock/app/index.js',
+  name: 'app'
+}]
+
+/* 
+EXEMPLO COM MULTIPLOS SERVIDORES
+module.exports = [{
+  script: '/home/flexdock/app/index.js',
+  name: 'app'
+}, {
+  script: '/home/flexdock/app2/index.js',
+  name: 'app2'
+}]
+*/
