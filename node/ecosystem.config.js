95c7c3978aef5aedd2e9657bc27baa8556c4ee41
flexpeak <59660805+flexpeak@users.noreply.github.com>
2022-07-28 15:01:20 -0400

    Update ecosystem.config.js

--- node/ecosystem.config.js
+++ node/ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = [{
-  script: '/home/flexdock/app/index.js',
+  script: '/var/www/app/index.js',
   name: 'app'
 }]
 
 /* 
 EXEMPLO COM MULTIPLOS SERVIDORES
 module.exports = [{
-  script: '/home/flexdock/app/index.js',
+  script: '/var/www/app/index.js',
   name: 'app'
 }, {
-  script: '/home/flexdock/app2/index.js',
+  script: '/var/www/app2/index.js',
   name: 'app2'
 }]
 */
